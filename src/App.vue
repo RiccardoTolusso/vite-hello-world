a233aadcb0770460f198387f90d2f7424240f34e
RiccardoTolusso <riccardotolusso215@gmail.com>
2024-05-30 00:28:12 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import AppTitleComponent from './components/AppTitle.vue';
+export default {
+    name:"Main",
+    data(){
+        return {
+            
+        }
+    },
+    methods:{
+
+    },
+    components:{
+        AppTitleComponent
+    }
+}
+</script>
+<template>
+
+    <AppTitleComponent/>
+</template>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name:"HelloWorld",
+    data(){
+        return {
+            title: 'La mia prima app con Vite!',
+        }
+    }
+}
+</script>
+<template>
+    {{ title }}
+</template>
